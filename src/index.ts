c9bd7a49b01df951f108fe2e8d65ea9847e90415
Oliver Huth <olivernhuthpriority@gmail.com>
2022-11-06 23:22:57 +0800

    Can Bypass new (V1) Measures

---
+++ src/index.ts
@@ -0,0 +1,367 @@
+import date from "date-and-time";
+
+import ReCaptchaPlugin from "puppeteer-extra-plugin-recaptcha";
+
+import puppeteer from "puppeteer-extra";
+
+import { Location } from "./Location";
+
+require("dotenv").config();
+
+console.log("CAPTCHA KEY: " + process.env.TWOCAPTCHA_KEY);
+
+const yargs = require("yargs/yargs");
+
+puppeteer.use(ReCaptchaPlugin());
+
+const { hideBin } = require("yargs/helpers");
+
+//const locationButtonXVal = 375;
+
+/*const locationButtonYValues = {
+  CAN: 516,
+  JNP: 540,
+  KELM: 563,
+  MDH: 589,
+  MID: 611,
+  MBK: 637,
+  ROCK: 661,
+  SUC: 685,
+  CTYW: 709,
+};*/
+
+if (process.env.TWOCAPTCHA_KEY === undefined) {
+  console.error("TWOCAPTCHA KEY NOT FOUND");
+  process.exit(1);
+}
+
+puppeteer.use(
+  ReCaptchaPlugin({
+    provider: {
+      id: "2captcha",
+      token: process.env.TWOCAPTCHA_KEY,
+    },
+  }),
+);
+
+const args = yargs(hideBin(process.argv))
+  .option("licenceNum", {
+    alias: "l",
+    description: "Drivers Licence number",
+    type: "string",
+  })
+  .option("expiryDate", {
+    alias: "e",
+    description: "Drivers Licence expiry date",
+    type: "string",
+  })
+  .option("firstName", {
+    alias: "f",
+    description: "Drivers First name",
+    type: "string",
+  })
+  .option("lastName", {
+    description: "Drivers Last name",
+    type: "string",
+  })
+  .option("dateOfBirth", {
+    alias: "d",
+    description: "Drivers date of birth",
+    type: "string",
+  })
+  .option("dateTop", {
+    //highest preffered date (e.g. 23/02/2005)
+    description: "Preffered highest date",
+    type: "string",
+  })
+  .option("dateBottom", {
+    //lowest preffered date (e.g. 01/02/2005)
+    description: "Preffered lowest date",
+    type: "string",
+  })
+  .option("location", {
+    description: "Preffered spot/location for driving test",
+    type: "string",
+  })
+  .option("debug", {
+    description: "set to TRUE to enable debug mode",
+    type: "string",
+  }).argv;
+
+//type KELM = "KELM"; type CAN = "CAN"; type MDH = "MDH"; type JNP = "JNP"; type ROCK = "ROCK"; type SUC = "SUC"; type MBK = "MBK"; type MID = "MID"; type CTYW = "CTYW";
+
+let isRegional = false;
+
+if (args.regional === "TRUE") isRegional = true;
+
+const rangeTop: Date = date.parse(`${args.dateTop} 6:00 PM`, "DD/MM/YYYY h:mm A");
+let rangeBottom: Date = date.parse(`${args.dateBottom} 6:00 AM`, "DD/MM/YYYY h:mm A");
+const pref1: Location = args.pref1;
+const pref2: Location = args.pref2;
+const pref3: Location = args.pref3;
+const pref4: Location = args.pref4;
+
+if (pref1 === undefined && pref2 === undefined && pref3 === undefined && pref4 === undefined) {
+  console.error("Enter a location preference");
+  process.exit(1);
+}
+
+const locations: Array<Location> = [pref1, pref2, pref3, pref4];
+
+console.log(locations);
+
+const userInfo = {
+  licenceNumber: args.licenceNum,
+  expiryDate: args.expiryDate,
+  firstName: args.firstName,
+  lastName: args.lastName,
+  dateOfBirth: args.dateOfBirth,
+};
+
+if (rangeBottom == null || rangeTop == null) {
+  if (rangeBottom == null) {
+    console.log("Lowest Date is Invalid!");
+    process.exit();
+  }
+  if (rangeTop == null) {
+    console.log("Highest Date is Invalid!");
+    process.exit();
+  }
+}
+
+console.log("Booking test for:");
+console.log(userInfo);
+console.log(`Between ${rangeBottom} to ${rangeTop}`);
+
+let GCaptchaResponse = ""; //holds solution to reCaptcha
+
+const isBooked: boolean = false;
+
+let debug: boolean = false;
+if (args.debug === "TRUE") debug = true;
+
+const browserargs = {
+  headless: !debug,
+};
+
+rangeBottom = formatDateRanges(rangeBottom, rangeTop);
+
+const startBot = async () => {
+  console.log("STARTING BOT");
+  try {
+    const browser = await puppeteer.launch(browserargs);
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1366, height: 768 });
+    await page.goto("https://online.transport.wa.gov.au/pdabooking/manage/?3").catch((err: any) => {
+      if (err.toString().includes("net::ERR_INTERNET_DISCONNECTED")) {
+        console.error("INTERNET NOT CONNECTED");
+        browser.close();
+        process.exit();
+      }
+    });
+
+    await page.setRequestInterception(true);
+
+    page.on("request", (request) => {
+      //method to intercept and edit authorisation requests
+      const method = request.method();
+      if (method == "POST") {
+        const postData = request.postData();
+        if (postData != undefined) {
+          if (postData.includes("id4_hf_0")) {
+            //checks for string unique to login auth form
+            /**?
+             * Edits form to include g-recaptcha-response token
+             */
+            let postDataFinal: string = postData.replace(
+              "&continue=1",
+              "&g-recaptcha-response=" + GCaptchaResponse,
+            );
+            postDataFinal = postDataFinal.concat("&continue=1");
+            request.continue({ postData: postDataFinal }); //injects postData with new form data
+            return;
+          }
+        }
+      }
+      request.continue();
+    });
+
+    const { solutions, error } = await page.solveRecaptchas(); //solves recaptchas and returns a solution/errors
+    if (error != undefined) {
+      console.error("RECAPTCHA ERROR");
+      throw error;
+    }
+    const solution = solutions[0].text;
+    if (solution != undefined) {
+      GCaptchaResponse = solution;
+    }
+
+    //Types in user info
+    await page.type('[name="clientDetailsPanel:licenceNumber"]', userInfo.licenceNumber);
+    await page.type("#licenceExpiryDatePicker", userInfo.expiryDate);
+    await page.type('[name="clientDetailsPanel:firstName"]', userInfo.firstName);
+    await page.type('[name="clientDetailsPanel:lastName"]', userInfo.lastName);
+    await page.type("#dateOfBirthPicker", userInfo.dateOfBirth);
+
+    //clicks page and waits for navigation
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click('[title="Submit the details and continue to the next page"]'),
+    ]);
+
+    //Checks if page has navigated
+    const pageNavCheck = await page.$$('[name="clientDetailsPanel:licenceNumber"]');
+    console.log("Logged in successfully");
+    if (pageNavCheck.length != 0) {
+      console.error("USER DETAILS INCORRECT");
+      return;
+    }
+
+    if (isRegional) {
+      await page.click('[value="REGIONAL"]'); //clicks regional radio button
+    }
+
+    await delay(1000);
+
+    //types upper and lower dates for booking
+    await page.type("#fromDateInput", date.format(rangeBottom, "DD/MM/YYYY"));
+
+    await delay(1000);
+
+    await page.type("#toDateInput", date.format(rangeTop, "DD/MM/YYYY"));
+
+    await delay(1000);
+
+    //Selects locations to look for
+    await page.evaluateHandle((locations) => {
+      locations.forEach((loc) => {
+        const doc = document.querySelector(`[value=${loc}]`) as HTMLElement;
+        doc.click();
+      });
+    }, locations);
+
+    const repeater = setInterval(async () => {
+      const checkSessionExpired = await page.$$("h2");
+      if (checkSessionExpired.length != 0) {
+        //session has expired
+        await page.close();
+        await browser.close();
+        console.log("Session ended, restarting.");
+        startBot(); //starts a new bot instance
+        clearInterval(repeater);
+        return;
+      }
+
+      await page.click('[title="Search"]'); //clicks button to get server to refresh information
+
+      await new Promise((r) => setTimeout(r, 500)).then(() => {
+        //waits for information to reach client
+        page.evaluate(() => document.querySelector("*")?.outerHTML); //gets  html from document
+      });
+
+      const times = await page.$$("#searchResultRadioLabel"); //list of elements containing dates needed
+
+      const dateText: Promise<string>[] = times.map((element) => {
+        // retrieves text form of dates from all html elements
+        return page.evaluate((el: any) => el.innerText, element); //this is a promise
+      });
+
+      const datesWithinRange = await Promise.all(dateText).then((dateText) => {
+        const datesWithinRange: boolean[] = dateText.map((text) => {
+          const splitDate = text.split(" "); //splits string into something like ["02/11/2022", "at", "11:35", "AM", ...]
+
+          const formattedDate = `${splitDate[0]} ${splitDate[2]} ${splitDate[3]}`; //creates a string that can be easily converted into a date
+
+          const elDate = date.parse(formattedDate, "DD/MM/YYYY h:mm A"); //parses textual date into a date object
+
+          const isDateInRange = checkDateInRange(elDate); // checks if date is in specified range of dates
+
+          return isDateInRange;
+        });
+        return datesWithinRange;
+      });
+
+      await datesWithinRange.every((bool, i) => {
+        if (isBooked === false) {
+          if (bool === true) {
+            bookDate(i);
+            clearInterval(repeater);
+            return false;
+          }
+        }
+      });
+      return 0;
+    }, 2000);
+
+    function bookDate(listNumber: number) {
+      page
+        .click(`#searchResultRadio${listNumber}`)
+        .then(() => page.click('[value="Confirm booking"]'))
+        .then(() => {
+          return page.waitForNavigation;
+        })
+        .then(() => {
+          console.log("FOUND BOOKING!");
+          return delay(10000);
+        })
+        .then(() => {
+          browser.close();
+        })
+        .catch((e) => {
+          console.log("Error booking date");
+          console.error(e);
+        });
+    }
+  } catch (e) {
+    console.error(e);
+    console.log("ERROR");
+    //clearInterval(infoRepeater)
+  } finally {
+  }
+  //await browser.close();
+};
+
+function checkDateInRange(dateListing: Date): boolean {
+  if (args.debug == "TRUE") {
+    console.info(date.subtract(rangeTop, dateListing).toHours());
+    console.info(date.subtract(dateListing, rangeBottom).toHours());
+  }
+  if (
+    date.subtract(rangeTop, dateListing).toHours() >= 0 &&
+    date.subtract(dateListing, rangeBottom).toHours() >= 0
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function formatDateRanges(rangeBottom: Date, rangeTop: Date): Date {
+  let newBottomDate: Date = rangeBottom;
+  //date selector logic
+  let currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + 1); //sets date 1 day into the future
+  /*
+          Difference between current date and lower bound of booking range
+          to make sure rangeBottom is not behind the current date
+          */
+  const lowerDiff = date.subtract(rangeBottom, currentDate);
+  const rangeDiff = date.subtract(rangeTop, rangeBottom);
+
+  if (lowerDiff.toMinutes() < 0) {
+    console.log("Setting lower date to current date as it is in the past");
+    newBottomDate = currentDate;
+  }
+  if (rangeDiff.toMinutes() <= 0) {
+    console.log("Please make your top date further than your bottom date");
+    process.exit(1);
+  }
+
+  return newBottomDate;
+}
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+startBot();
